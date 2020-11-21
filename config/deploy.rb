6f6db1499702f3b6e0b9b515632262c0f441e517
shitangdama <kbr1990117@gmail.com>
2020-11-21 20:46:04 +0800

    init_code

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,7 +18,7 @@ set :deploy_to, "/home/ubuntu/viki"
 # set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
 
 # Default value for :pty is false
-# set :pty, true
+set :pty, true
 
 # Default value for :linked_files is []
 append :linked_files, 'config/database.yml', 'config/master.key'
@@ -33,7 +33,22 @@ append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/syst
 # set :local_user, -> { `git config user.name`.chomp }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 5
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
+
+set :deploy_via,      :remote_cache
+
+
+set :puma_conf,       "#{shared_path}/puma.rb"
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/puma.sock" 
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.error.log"
+set :puma_error_log,  "#{release_path}/log/puma.access.log"
+set :puma_threads, [0, 16]
+set :puma_workers, 0
+set :puma_preload_app, true
+set :puma_worker_timeout, nil
+set :puma_restart_command, 'bundle exec puma'

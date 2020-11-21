0861e9d1e9b32cd74168b572380e334ef569c959
shitangdama <kbr1990117@gmail.com>
2020-11-21 22:51:45 +0800

    init_code

--- config/deploy.rb
+++ config/deploy.rb
@@ -41,14 +41,30 @@ set :keep_releases, 5
 set :deploy_via,      :remote_cache
 
 
-set :puma_conf,       "#{shared_path}/puma.rb"
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/puma.sock" 
-set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
-set :puma_access_log, "#{release_path}/log/puma.error.log"
-set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :puma_threads, [0, 16]
-set :puma_workers, 0
+set :puma_rackup, -> { File.join(current_path, 'config.ru') }
+set :puma_state, -> { "#{shared_path}/tmp/pids/puma.state" }
+set :puma_pid, -> { "#{shared_path}/tmp/pids/puma.pid" }
+set :puma_bind, -> { "unix://#{shared_path}/tmp/sockets/puma.sock" }
+set :puma_conf, -> { "#{shared_path}/puma.rb" }
+set :puma_access_log, -> { "#{shared_path}/log/puma.log" }
+set :puma_error_log, -> { "#{shared_path}/log/puma.err.log" }
+set :puma_role, :app
+set :puma_env, -> { fetch(:rack_env, fetch(:rails_env, 'production')) }
+set :puma_threads, [4, 16]
+set :puma_workers, 4
+set :puma_worker_timeout, 30
+set :puma_init_active_record, true
 set :puma_preload_app, true
-set :puma_worker_timeout, nil
-set :puma_restart_command, 'bundle exec puma'
+set :puma_prune_bundler, true
+
+namespace :puma do
+  desc 'Create Directories for Puma Pids and Socket'
+  task :make_dirs do
+    on roles(:app) do
+      execute "mkdir #{shared_path}/tmp/sockets -p"
+      execute "mkdir #{shared_path}/tmp/pids -p"
+    end
+  end
+
+  before :start, :make_dirs
+end
